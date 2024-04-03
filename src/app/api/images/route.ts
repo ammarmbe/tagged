a426a10afa32aef6bfa5d3083b3c5e73f1b8ca2d
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-04-03 23:40:33 +0200

    updated images

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -4,9 +4,10 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const item_id = searchParams.get("item_id");
 
-  const data = await sql("SELECT * from item_images WHERE item_id = $1", [
-    item_id,
-  ]);
+  const data = await sql(
+    "SELECT * from item_images WHERE item_id = $1 ORDER BY thumbnail DESC NULLS LAST",
+    [item_id],
+  );
 
   return new Response(JSON.stringify(data));
 }
